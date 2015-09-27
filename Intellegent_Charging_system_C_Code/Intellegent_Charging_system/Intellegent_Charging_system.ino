56078c9ab36b15343d15b599255203c084a74287
BRAD <b83moon-@hotmail.com>
2015-09-27 17:03:44 -0600

    started arduino voltage detecting code for charging/discharging

---
+++ Intellegent_Charging_system_C_Code/Intellegent_Charging_system/Intellegent_Charging_system.ino
@@ -0,0 +1,60 @@
+
+
+#define CELLALL 0
+#define CELL1 1
+#define CELL2 2
+#define CELL3 3
+#define CELL4 4
+
+void setup () {
+  
+  pinMode(CELL1, INPUT);
+  pinMode(CELL2, INPUT);
+  pinMode(CELL3, INPUT);
+  pinMode(CELL4, INPUT);
+  // pinMode(CELLALL, INPUT);
+  
+  int voltage1, voltage2, voltage3, voltage4, voltageAll = 0;
+
+  Serial.begin(9600);
+}
+
+
+void loop () {
+  
+  // Measure voltages
+  
+  voltage1 = (5/1023) * analogRead(CELL1);
+  voltage2 = (5/1023) * analogRead(CELL2);
+  voltage3 = (5/1023) * analogRead(CELL3);
+  voltage4 = (5/1023) * analogRead(CELL4);
+  voltageAll = voltage1 + voltage2 + voltage3 + voltage4;
+  
+  // Send data over serial connection
+  Serial.print(voltage1);
+  Serial.print("*");
+  Serial.print(voltage2);
+  Serial.print("*");
+  Serial.print(voltage3);
+  Serial.print("*");
+  Serial.print(voltage4);
+  Serial.print("*");
+  Serial.print(voltageAll);
+  Serial.println("");
+
+  
+  
+}
+
+void ChargeCell(int cellNumber) {
+  
+  
+}
+
+int CellVoltage() {
+  
+}
+
+boolean LogData() {
+  
+}
