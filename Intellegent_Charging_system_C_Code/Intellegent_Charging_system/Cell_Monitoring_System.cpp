b3066a98ce07299e203d8c23a96696508ef62f75
BRAD <b83moon-@hotmail.com>
2015-11-25 15:16:32 -0700

    started new .cpp file for the cell monitoring system

---
+++ Intellegent_Charging_system_C_Code/Intellegent_Charging_system/Cell_Monitoring_System.cpp
@@ -0,0 +1,88 @@
+
+
+// Initial Arduino Mega Pins
+#define CELL1 0   //Analog Pins 0-7 connected to each cell cathode
+#define CELL2 1
+#define CELL3 2
+#define CELL4 3
+#define CELL5 4 
+#define CELL6 5
+#define CELL7 6
+#define CELL8 7
+
+
+/******************** Initialization *****************/
+void setup() {
+  //Initailize variables
+  int cellVoltage[8]= 0;  
+  int dataNumber = 0;
+  int cellNumber = 1;
+  char input;
+  
+  //Initialize pin modes
+  pinMode(INPUT, CELL1);
+  pinMode(INPUT, CELL2);
+  pinMode(INPUT, CELL3);
+  pinMode(INPUT, CELL4);
+  pinMode(INPUT, CELL5);
+  pinMode(INPUT, CELL6);
+  pinMode(INPUT, CELL7);
+  pinMode(INPUT, CELL8); 
+
+
+  /************* Serial Connection Initailization ******/
+  serial.begin(9600);   //Start Serail Connection with Matlab
+  serial.print("*\n");   // Tell matlab to begin with * command
+  while(input != "*") {   // wait for handshake signal * from matlab to begin 
+    input = serial.read();
+  }
+}
+
+
+
+/************************ Main Loop Starts Here*********************/
+void loop() {
+  for(cellNumber = 0; cellNumber < 7; cellNumber++) { 
+    switch (cellNumber) {
+      case 0:  
+        cellVoltage[cellNumber] = analogRead(CELL1);
+      case 1:
+        cellVoltage[cellNumber] = analogRead(CELL2);
+      case 2:
+        cellVoltage[cellNumber] = analogRead(CELL3);
+      case 3:
+        cellVoltage[cellNumber] = analogRead(CELL4);
+      case 4:
+        cellVoltage[cellNumber] = analogRead(CELL5);
+      case 5:
+        cellVoltage[cellNumber] = analogRead(CELL6);
+      case 6:
+        cellVoltage[cellNumber] = analogRead(CELL7);
+      case 7:
+        cellVoltage[cellNumber] = analogRead(CELL8);
+    }
+  }
+  
+  for(i = 0; i < 7; i++) {
+    serial.write(i);
+    serial.write("*");
+    serial.write(cellVoltage[i]);
+    serial.write("*");
+    serial.write(dataNumber);
+    serial.write("^");
+  }
+  
+  dataNumber++;
+  delay(1000);
+  cellNumber = 0;
+ 
+  for(i = 0; i < 7; i++) {
+    cellVoltage[i] = 0; 
+  }
+}
+
+  
+  
+  
+  
+  
