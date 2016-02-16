82fbdd549ed3e925354968dff86694b040df240f
BRAD <b83moon-@hotmail.com>
2016-02-16 01:05:07 -0700

    Reoragized folders

--- Cell Balancing Code/CellBalancer/CellBalancer.ino
+++ Cell Balancing Code/CellBalancer/CellBalancer.ino
@@ -3,104 +3,124 @@
 // Cell Balancing System                              //
 // Based on measured current, controls DCDC Converter //
 // Jan 11, 2016                                       //
-// Sean Girard                                        //
+// Sean Girard, Brad Moon                             //
 /******************************************************/
 
-
-#define Current_Sensor 3
-#define DC_Converter 10
+#include <Wire.h>
+#include <MCP3221.h>
+#define MOSFET 10
 #define LED 13
 
-float Current_thresh = 0.5; // [A]
-float analogCurrent = 0;
-float current = 0;
-float sensitivity = 0.0185; // [V/A]
+// Define Importanct Variables
+const byte i2cAddress = 0x4E;  
+const float currentThresh = 0.5; // [A]
 int IterationNumber = 0;
-float rawADC = 0;
+const int I2CadcVRef = 4880;
+MCP3221 i2cADC(i2cAddress, I2CadcVRef);
 
 
+
+
+/*************************** Initailize Setup **********************************/
+/*******************************************************************************/
 void setup() {
-  
+    
   // Set up pins
-  pinMode(Current_Sensor, INPUT);
-  pinMode(DC_Converter, OUTPUT);
+  pinMode(MOSFET, OUTPUT);
   pinMode(LED, OUTPUT);
 
   // Set up Serial Connection
   Serial.begin(9600);
   Serial.println("Setup Complete.");
 
-  // Enable DCDC Converter
-  digitalWrite(DC_Converter, HIGH);
+  // Setup I2C Connection
+  Wire.begin();
+
+  // Enable MOSFET for main Circuit Loop
+  digitalWrite(MOSFET, HIGH);
   digitalWrite(LED, HIGH);
 
   // Allow time for DC_Converter to turn on
-  delay(5000);
+  delay(2000);
   Serial.println("DC_Converter ON");
   Serial.println("");
+
+  //Initailize Loop
+
 }
 
 
+/*************************** Start Main Loop ***********************************/
+/*******************************************************************************/
 
 void loop() {
-
-  // To measure the current, first note the following:
-  //  1. Arduino measures 0 to 1024 (only up to 675.84 is utilized)
-  //  2. Current Sensor outputs 0 to 3.3V
-  //  3. Current Sensor reading of 1.65V, corresponds to 0A measured
-  //  4. Current Sensor has 18.5mv/A Sensitivity (0.0185 V/A)
-  //
-  // |--------------|--------------|                [Current]
-  // -75A           0A             75A
-  //
-  // |--------------|--------------|                [Current Sensor Output]
-  // 0             1.65V          3.3V
-  // 
-  // |--------------|--------------|-----------|    [Arduino Analog input]
-  // 0           337.92          675.84       1024
-  //
-  // Example: analogRead measures 400
-  //  1. 400 / 675.84 = 0.59
-  //  2. 3.3 * 0.59 = 1.95V is the Current Sensor output (0 - 3.3V)
-  //  3. 1.95V - 1.65V = 0.3V above the 0A point on Current Sensor scale
-  //  4. 0.3V / 0.0185 V/A = 16A is the current
-
-  current = CurrentMeasure();
+  int current = CurrentMeasure();
+  delay(2000);
   
   // Disable DC_Converter when current falls below cut-off
-  if (current < Current_thresh) {
+  if (avgCurrent < currentThresh) {
     IterationNumber++;
-    delay(250);
     Serial.print("Current Threshold Reached : ");
     Serial.print(IterationNumber);
     Serial.println(" times.");
-      if (IterationNumber == 3){
-      digitalWrite(DC_Converter, LOW);
+    if (IterationNumber == 3){
+      digitalWrite(MOSFET, LOW);
       digitalWrite(LED, LOW);
       Serial.println("Current cut-off point reached.");
-      Serial.println("DC Converter OFF");
-      while (true);
+      Serial.println("Cut off Circuit Loop");
+      while (true){
+        int currentMeasure();
+        if (current > 0.00001 ) {
+          Serial.println("Warning! MOSFET Failed to turn off circuit!");
+          delay(1000);
+        } else {
+          Serial.println("MOSFET Porperly Turned off Circuit :)");
+          while (true);
+        }
+      }
     }
   } else {
     IterationNumber = 0;
-  }
+  } 
 }
 
 
+/*************************** Current Mesure Function ***************************/
+/*******************************************************************************/
 float CurrentMeasure() { 
- //   Read & print ADC output
-  int rawADC = analogRead(Current_Sensor);
+  // Define Variables
+  float avgCurrent = 0;
+  int avgAdcRaw = 0;
+  float avgVoltage = 0;
+  float current = 0;
+  int adcRaw = 0;
+  float voltage = 0;
+  // Read & print ADC output
+   adcRaw = i2cADC.readI2CADC();
+   avgAdcRaw = i2cADC.calcRollingAVG(); 
   Serial.print("ADC: ");
-  Serial.print(rawADC);
-  Serial.println("/1024.");
+  Serial.print(adcRaw);
+  Serial.println("/4096.");
 
   // Calculate & print current
-  float current = (rawADC/1024)*5.0000000000000000000;
+  //Average
+  avgVoltage = (avgAdcRaw/4096.0)*I2CadcVRef/1000.0;
+  Serial.print("Average Voltage: ");
+  Serial.print(avgVoltage, 8);
+  Serial.println(" V.");
+  avgCurrent = avgVoltage/0.3;
+  Serial.print("Average Current: ");
+  Serial.print(avgCurrent, 8);
+  Serial.println(" A.");
+  //Instantaneous
+  voltage = (adcRaw/4096.0)*I2CadcVRef/1000.0;
+  Serial.print("Voltage: ");
+  Serial.print(voltage, 8);
+  Serial.println(" V.");
+  current = voltage/0.3;
   Serial.print("Current: ");
   Serial.print(current, 8);
   Serial.println(" A.");
   Serial.println();
   return current;
 }
-
-// End of file.
